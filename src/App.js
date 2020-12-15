e33d1e3db86ef7916dd809a62130d1a0dc048775
Pratima Gautam <pratimagautam977@gmail.com>
2020-12-15 10:03:49 +0545

    changes

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
           <Nav />
           <div className="row">
             <div className="col-6">
-              <h3>Add Movie</h3>
+              <h3>Add Movies</h3>
               <AddMovie />
             </div>
             <div className="col-6">
